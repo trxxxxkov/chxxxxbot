815a4ddd75d4505a8201d0a4c113800b0269828e
trxxxxkov <trxxxxkov@gmail.com>
2024-07-05 20:34:46 +0200

    Add new payment-related commands

--- src/templates/bot_menu.py
+++ src/templates/bot_menu.py
@@ -2,13 +2,19 @@ bot_menu = {
     "ru": {
         "draw": "Создать изображение по предложенному описанию",
         "forget": "Удалить последние сообщения из памяти",
-        "balance": "Показать баланс и информацию о способах оплаты",
+        "balance": "Показать баланс и информацию о стоимости использования",
         "help": "Показать описание команд и общую информацию о боте",
+        "donate": "Пополнить счёт, используя Telegram Stars",
+        "refund": "Оформить возврат платежа с использованием Telegram Stars",
+        "paysupport": "Информация о способах оплаты и возвратах",
     },
     "en": {
         "draw": "Generate an image by description",
         "forget": "Clear previous messages from Sebastian's memory",
-        "balance": "Show your balance and info about payment methods",
+        "balance": "Show your balance and information about usage cost",
         "help": "Show a description of the commands and general info",
+        "donate": "Top up your account using Telegram Stars",
+        "refund": "Make a refund of a payment made with Telegram Stars",
+        "paysupport": "Show information about payment methods and refunds",
     },
 }
