68da9fa87b80b9b1d0a1740aa9e712af16f5aa7e
trxxxxkov <trxxxxkov@gmail.com>
2024-10-18 23:56:16 +0200

    Set the repository to an initial state.

--- src/main.py
+++ src/main.py
@@ -1,79 +1,58 @@
-"""Bot entrypoint"""
-
+import asyncio
 import logging
 import sys
 from os import getenv
 
-from aiohttp import web
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-from aiogram import Bot, Dispatcher
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("BOT_TOKEN")
 
-from src.handlers import (
-    public_cmds,
-    hidden_cmds,
-    privileged_cmds,
-    callbacks,
-    other_upds,
-)
-from utils.globals import (
-    bot,
-    BASE_WEBHOOK_URL,
-    WEBHOOK_PATH,
-    WEBHOOK_SECRET,
-    WEB_SERVER_HOST,
-    WEB_SERVER_PORT,
-)
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
 
 
-async def on_startup(bot: Bot) -> None:
-    # If you have a self-signed SSL certificate, then you will need to send a public
-    # certificate to Telegram
-    await bot.set_webhook(
-        f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
-        secret_token=WEBHOOK_SECRET,
-        allowed_updates=[
-            "message",
-            "inline_query",
-            "callback_query",
-            "pre_checkout_query",
-        ],
-    )
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-def main() -> None:
-    # Connect debugger depending on a variable in .env
-    if getenv("BOT_DEBUG") != "0":
-        import debugpy
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
 
-        debugpy.listen(("0.0.0.0", 5678))
-    # Dispatcher is a root router
-    dp = Dispatcher()
-    # All handlers should be attached to the Router (or Dispatcher).
-    # The order of the routers matters
-    dp.include_routers(
-        other_upds.rt, callbacks.rt, privileged_cmds.rt, hidden_cmds.rt, public_cmds.rt
-    )
-    # Register startup hook to initialize webhook
-    dp.startup.register(on_startup)
-    # Create aiohttp.web.Application instance
-    app = web.Application()
-    # Create an instance of request handler,
-    # aiogram has few implementations for different cases of usage
-    # In this example we use SimpleRequestHandler which is designed to handle simple cases
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=WEBHOOK_SECRET,
-    )
-    # Register webhook handler on application
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-    # Mount dispatcher startup and shutdown hooks to aiohttp application
-    setup_application(app, dp, bot=bot)
-    # And finally start webserver
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    main()
+    asyncio.run(main())
