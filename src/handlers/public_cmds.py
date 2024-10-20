3c0ce948e0867b7f11d3b6553968876cf6013124
trxxxxkov <trxxxxkov@gmail.com>
2024-10-20 20:02:33 +0200

    Add forget_handler()

--- src/handlers/public_cmds.py
+++ src/handlers/public_cmds.py
@@ -9,26 +9,33 @@ import aiogram
 from src.openai import openai_globals
 from src.utils import bot_globals
 
+
 rt = aiogram.Router()
+
+
+@rt.message(aiogram.filters.Command("forget"))
+async def forget_handler(message: aiogram.types.Message) -> None:
+    """Delete current user's thread and crate new one instead."""
+    user = bot_globals.bot_users[message.from_user.id]
+    await openai_globals.client.beta.threads.delete(user["thread_id"])
+    empty_thread = await openai_globals.client.beta.threads.create()
+    bot_globals.bot_users[message.from_user.id]["thread_id"] = empty_thread
 
 
 @rt.message()
 async def default_handler(message: aiogram.types.Message) -> None:
     """Process all unhandled updates."""
+    await bot_globals.bot.send_chat_action(
+        message.chat.id, aiogram.enums.ChatAction.TYPING
+    )
     user = bot_globals.bot_users[message.from_user.id]
-    message = await openai_globals.client.beta.threads.messages.create(
+    await openai_globals.client.beta.threads.messages.create(
         thread_id=user["thread_id"],
         role="user",
         content=message.text,
     )
-    run = await openai_globals.client.beta.threads.runs.create_and_poll(
-        thread_id=user["thread_id"],
-        assistant_id=user["assistant_id"],
+    stream = await openai_globals.client.beta.threads.runs.create(
+        thread_id=user["thread_id"], assistant_id=user["assistant_id"], stream=True
     )
-    if run.status == "completed":
-        messages = await openai_globals.client.beta.threads.messages.list(
-            thread_id=user["thread_id"]
-        )
-        print(messages)
-    else:
-        print(run.status)
+    async for event in stream:
+        print(event)
--- src/utils/user_management.py
+++ src/utils/user_management.py
@@ -7,7 +7,7 @@ from src.openai import openai_globals, assistants
 async def user_initialization(new_user_id: int, new_user_lang: str) -> None:
     """Activate new user's account and grant him a welcome gift."""
     empty_thread = await openai_globals.client.beta.threads.create()
-    default_assistant = await assistants.get_assistant(name="gpt-4o-mini")
+    default_assistant = await assistants.get_assistant(name="Default gpt-4o-mini")
     bot_globals.bot_users[new_user_id] = {
         "assistant_id": default_assistant.id,
         "thread_id": empty_thread.id,
